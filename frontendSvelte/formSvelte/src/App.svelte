f9c2daae4fb1a46de79b3ef68d3bdb03cbaf8e01
DIONISI GREGORY AARON <77567516@castelli.local>
2025-02-18 10:46:20 +0100

    Versione finale

--- frontendSvelte/formSvelte/src/App.svelte
+++ frontendSvelte/formSvelte/src/App.svelte
@@ -5,7 +5,8 @@
     async function sendName(event) { //visto che deve fare una fecth deve essere asincrona
         event.preventDefault();
         //console.log(name);
-        const res = await fetch("http://localhost:3000/utente", { //copiamo l'end point da frontend/index.html
+        try {
+            const res = await fetch("http://localhost:3000/utente", { //copiamo l'end point da frontend/index.html
             method: "POST",
             headers: {
                 "Content-Type" : "application/json"
@@ -20,7 +21,10 @@
         } else {
             responseMessage = "Errore";
         }
+    }catch(error) {
+        console.log("Errore di rete.");
     }
+}
 </script>
 
 
