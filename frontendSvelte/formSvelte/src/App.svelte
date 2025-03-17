91dbad84e41b1cb0b35efe75d0f8e6e4102d71cc
GregoryDionisi <gregoryd324@gmail.com>
2025-03-17 20:04:26 +0100

    Versione finale

--- frontendSvelte/formSvelte/src/App.svelte
+++ frontendSvelte/formSvelte/src/App.svelte
@@ -1,30 +1,51 @@
 <!--Utilizziamo svelte e non svelte-kit perchè quest'ultimo è fullstack e quindi comprende anche il backend-->
 <script>
+    import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
+
     let questions = $state([]);
     let results = $state();
-
+    
     let formState = $state({
         answers: {},
         step: 0,
         error: "",
     }); //stato utilizzato per verificare se ci sono le risposte e quindi per poter andare avanti
 
+    let loading = false;
+
+    //array di animazioni Lottie per ogni step
+    const lottieAnimations = [
+        "https://lottie.host/c084d070-1921-4e2b-b1fb-83d7797f256b/jRXJCBlRZS.lottie", //animazione per lo step 1
+        "https://lottie.host/7354322b-357b-48fa-94fa-09242f5bd1f3/ov3rLQErch.lottie",
+        "https://lottie.host/cc7e055e-0734-48fe-a856-c40b4aa0a63d/GHZAcSSG1s.lottie",
+        "https://lottie.host/288e725b-8808-405e-961e-bc64f90f7153/nT2XSX7PGk.lottie",
+        "https://lottie.host/9a3f9537-ece6-42b6-8efc-2e9e245ee15b/fiWUPMFXTT.lottie",
+        "https://lottie.host/20954e73-7f8a-4304-aea1-fcfa2596e1c5/VwpA9jpXib.lottie"
+    ];
+    
+    const loopAnimations = [false, true, true, true, true, true];  //la prima animazione in loop, la seconda no
 
     function nextStep(id) {
+        console.log("Step change:", formState.step); //debug
         if (formState.answers[id]) {
-            if (formState.step < questions.length - 1) { //per vedere qual è l'ultima domanda
-                formState.step += 1;
-                formState.error = "";
+            if (formState.step < questions.length - 1) {
+                loading = true; // Inizia il caricamento
+                setTimeout(() => {
+                    formState.step += 1;
+                    formState.error = "";
+                    animationKey++;
+                    loading = false;
+                }, 1000);
             } else {
                 formState.step += 1;
                 formState.error = "";
-                //all'ultima domanda invia le risposte
                 sendAnswers();
             }
         } else {
             formState.error = `Please enter ${id}`;
         }
     }
+
 
     function resetForm() {
         formState.answers = {};
@@ -96,10 +117,27 @@ async function fetchResults() {
 
 
 <h1>Form Page</h1>
- 
 <h1>
     {formState.answers?.name ? formState.answers?.name : "User"}'s Form
 </h1>
+<div class="container">
+    <div class="lottie-container">
+        {#if loading}
+            <div class="loading-overlay">
+                <DotLottieSvelte
+                    src="https://lottie.host/661906f7-583a-4c29-8204-36f923b5a4dd/GBcISFHQ0z.lottie"
+                    autoplay
+                    loop={true}
+                    style="width: 100%; height: 100%;"/>
+            </div>
+        {/if}
+        <DotLottieSvelte
+            src={lottieAnimations[formState.step]}
+            autoplay
+            loop={loopAnimations[formState.step]}
+            style="width: 100%; height: 100%;" />
+    </div>
+</div>
 {#if formState.step >= questions.length}
     <p>Thank you for your answers, {formState.answers.name}!</p>
     <!-- Riepilogo risposte -->
@@ -187,6 +225,39 @@ async function fetchResults() {
     {/if}
 </div>
 {/snippet}
+
+<style>
+    .lottie-container {
+        width: 400px; //dimensioni animazione
+        height: 400px;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+    }
+
+    .container {
+        height: 25vh;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .loading-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
 
 
 
